566f254017ce30d0d81e0068b716361805dc63f9
112lenca112 <72919798+112lenca112@users.noreply.github.com>
2020-11-15 16:26:02 +0100

    Initial check-in of module ProjectPortfolio2_Group11

---
+++ WebService/Controller/BookmarkController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using DataServiceLib;
+using DataServiceLib.DBObjects;
+using Microsoft.AspNetCore.Mvc;
+using ProjectPortfolio2_Group11.Model;
+
+
+namespace ProjectPortfolio2_Group11.Controller
+{
+    [ApiController]
+    [Route("api/bookmark")]
+    public class BookmarkController : ControllerBase
+    {
+        private readonly DataServiceFacade _dataService;
+        private readonly IMapper _mapper;
+        public BookmarkController(DataServiceFacade dataService, IMapper mapper)
+        {
+            _dataService = dataService;
+            _mapper = mapper;
+        }
+         
+
+            [HttpGet("{userid}/{nconst}")]
+
+            // uses userid and nconst as the URL path
+       
+        public IActionResult GetBookMark(int userid,string nconst)
+        {
+            var bookmark = _dataService.BookmarkingDS.GetBookMark(userid,nconst);
+            if (bookmark == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<BookmarkPersonDto>(bookmark));
+        }
+
+
+        [HttpPost("{userid}")]
+        public IActionResult CreateBookmark(BookmarkPersonForCreationDto bookDto)
+        {
+            var bookmark = _mapper.Map<BookmarkPerson>(bookDto);
+            _dataService.BookmarkingDS.CreateBookmark(bookmark);
+            return Created("", bookmark);
+        }
+
+
+        [HttpPut("{userid}/{nconst}")]
+        public IActionResult UpdateBookmark(int UserId,string Nconst)
+        {
+            var bookmark = _mapper.Map<BookmarkPerson>(UserId);
+            if (_dataService.BookmarkingDS.UpdateBookmark(bookmark))
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+
+
+        [HttpDelete("{userid}/{nconst}")]
+        public IActionResult DeleteBookmark(int userid,string nconst)
+        {
+            var response = " bookmark not found";
+            if (!_dataService.BookmarkingDS.DeleteBookmark(userid,nconst))
+            {
+                return NotFound(response);
+            }
+            response = " bookmark deleted";
+            return CreatedAtRoute(null, userid + response);
+        }
+    }
+}
---
+++ WebServiceTests/BookmarkControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using AutoMapper;
+using DataServiceLib.IDataService;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ProjectPortfolio2_Group11.Controller;
+using DataServiceLib.IDataService;
+using Xunit;
+
+
+namespace WebServiceTests
+{
+   
+    public class BookmarkControllerTest
+    {
+        private Mock<IBookmarkingDataService> _BookmarkingDataServiceMock;
+        private Mock<IMapper> _mapperMock;
+        private Mock<IUrlHelper> _urlMock;
+
+
+        public BookmarkControllerTest()
+        {
+
+             _BookmarkingDataServiceMock = new Mock<IBookmarkingDataService>();
+             _mapperMock = new Mock<IMapper>();
+             _urlMock = new Mock<IUrlHelper>();
+
+        }
+
+        [Fact]
+        public void GetProductWithValidIdShouldReturnOk()
+        {
+
+
+            _BookmarkingDataServiceMock.Setup(x => x.GetBookMark(1)).Returns(new BookMark { nconst = new nconst() });
+            _mapperMock.Setup(x => x.Map<BookmarkPerson>(It.IsAny<BookMark>())).Returns(new BookMarkPerson());
+
+
+            var ctrl = new BookmarkController(_BookmarkingDataServiceMock.Object, _mapperMock.Object);
+            ctrl.Url = _urlMock.Object;
+
+            var response = ctrl.GetBookMark(1);
+
+            response.Should().BeOfType<OkObjectResult>();
+
+        }
+
+        [Fact]
+        public void GetProductWithInvalidIdShouldReturnNotFound()
+        {
+
+            var ctrl = new BookmarkController(_BookmarkingDataServiceMock.Object, null);
+
+            var response = ctrl.GetBookMark(10);
+
+            response.Should().BeOfType<NotFoundResult>();
+        }
+    }
+}
